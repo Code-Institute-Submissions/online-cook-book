1ae1e8cf8323ccc1a8e77dec51112bba82786f5e
Sulejman Ljimaj <sulejman-limaj@hotmail.be>
2019-03-15 10:30:57 +0000

    minor changes-added js count function-added edit function-added upvote function

--- app.py
+++ app.py
@@ -65,29 +65,42 @@ def add_recipes():
 @app.route('/edit_page')
 def edit_page():
     return render_template('edit_page.html', recipes = mongo.db.recipes.find())
-    
-@app.route('/edit_recipe')
+
+@app.route('/edit_page_form/<recipe_id>')
+def edit_page_form(recipe_id):
+    return render_template('edit_page_form.html', recipes = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)}))
+
+#edit recipe function
+@app.route('/edit_recipe/<recipe_id>', methods=["POST"])
 def edit_recipe(recipe_id):
     recipes = mongo.db.recipes
-    recipes.update({"_id": ObjectId(recipe_id)}, {
+    recipes.update({"_id": ObjectId(recipe_id)}, 
+    {
         "meal_name": request.form.get("meal_name"),
         "preparation": request.form.get("preparation"),
         "description": request.form.get("description"),
         "author": {
-                "last_name": request.form.get("last_name"),
-                "name": request.form.get("name")
-            },
+            "last_name": request.form.get("last_name"),
+            "name": request.form.get("name")
+        },
         "category_course": request.form.get("category_course"),
-        "ingredients":request.form.get("ingredients"),
-        "macros":{
-                    "fat":request.form.get("fat"),
-                    "calories":request.form.get("calories"),
-                    "proteins":request.form.get("proteins"),
-                    "carbs":request.form.get("carbs")
-                },
+        "ingredients": request.form.get("ingredients"),
+        "macros": {
+            "fat": request.form.get("fat"),
+            "calories": request.form.get("calories"),
+            "proteins": request.form.get("proteins"),
+            "carbs": request.form.get("carbs")
+        },
         "url_img": request.form.get("url_img")
     })
+    return redirect(url_for('get_recipes'))
     
+@app.route('/upvote/<recipe_id>', methods=["POST"])
+def upvote(recipe_id):
+    recipes=mongo.db.recipes
+    recipes.update({"_id": ObjectId(recipe_id)},{"$inc": {"upvote": request.form("")}})
+    return redirect(url_for("get_recipes"))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
---
+++ static/count.js
@@ -0,0 +1,5 @@
+var count = 1;
+function upvoteByOne() {
+  document.getElementById("#modify").value = "Like " +count;
+  count++;
+}
